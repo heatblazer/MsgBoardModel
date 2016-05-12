89e170cf0806292503c382ac5156fcf774444b62
ilianzapryanov <ilian.zapryanov@balkantel.net>
2016-05-12 14:39:49 +0300

    src: added new members m_clicked for testing

--- types.cpp
+++ types.cpp
@@ -4,8 +4,9 @@
 namespace msgboard
 {
 
-Msg::Msg(const QString& msg, MsgType type, int timer)
-    : m_msg(msg), m_type(type), m_timer(timer), m_index(-1)
+Msg::Msg(const QString& msg, MsgType type, int timer, bool clicked)
+    : m_msg(msg), m_type(type), m_timer(timer), m_clicked(clicked),
+      m_index(-1)
 {
 
 }
