a4b784bae5373b5a6b24e668d8034e0a27298f11
ilianzapryanov <ilian.zapryanov@balkantel.net>
2016-05-12 14:41:18 +0300

    src: added tests

--- main.cpp
+++ main.cpp
@@ -27,10 +27,20 @@ int main(int argc, char *argv[])
     myView->setMsgModel((QAbstractItemModel*)myModel);
     \
     myModel->addTimerMsg("128381231283123", 2000);
+    myModel->addTimerMsg("128381231283123", 12000);
+    myModel->addTimerMsg("128381231283123", 1000);
+    myModel->addTimerMsg("128381231283123", 33000);
+
     myModel->addUserMsg("dfsdfgfgfggghgh");
+    myModel->addUserMsg("dfsdfgfgfggghgh");
+    myModel->addUserMsg("dfsdfgfgfggghgh");
+    myModel->addUserMsg("dfsdfgfgfggghgh");
+    myModel->addUserMsg("dfsdfgfgfggghgh");
+
     myModel->addStaticMsg("------2-2--2-2-2-");
 
     myView->showMsgModel();
+
 
     return a.exec();
 }
