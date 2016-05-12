a989f6dce2b56bc792cd510bc36740297dccb760
ilianzapryanov <ilian.zapryanov@balkantel.net>
2016-05-12 14:39:05 +0300

    src: added iostream for test messages also connected the click signals

--- msgboardview.cpp
+++ msgboardview.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+
 #include "msgboardveiw.h"
 #include "types.h"
+
 
 namespace msgboard
 {
@@ -9,6 +12,7 @@ MsgBoardView::MsgBoardView(QWidget *parent)
 {
     // this enables emits to a clicked(QMOdelIndex& ind); //
     this->setAttribute(Qt::WA_AcceptTouchEvents, true);
+
 }
 
 MsgBoardView::~MsgBoardView()
@@ -19,7 +23,19 @@ MsgBoardView::~MsgBoardView()
 void MsgBoardView::setMsgModel(QAbstractItemModel* model)
 {
     //((QListView*)this)->setModel(model);
-    setModel(model);
+    m_model = (MsgBoardModel*) model;
+    setModel(m_model);
+
+
+    std::cout << "CUrrent index clicked: "
+              <<currentIndex().row()
+             << std::endl;
+
+    // this will enable the model to recieve the clicks
+
+    connect(this, SIGNAL(clicked(QModelIndex)),
+            m_model, SLOT(hMsgHideUserActivity()));
+
 }
 
 // use the parent`s show
@@ -28,5 +44,6 @@ void MsgBoardView::showMsgModel()
     //((QListView*)this)->show();
     show();
 }
+
 
 }
