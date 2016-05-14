5d963b38f61c7f4447d313326b9c3352fd746bb4
ilianzapryanov <ilian.zapryanov@balkantel.net>
2016-05-14 12:24:55 +0300

    src: added style builder :)

--- types.cpp
+++ types.cpp
@@ -16,4 +16,73 @@ Msg::~Msg()
 
 }
 
+
+MsgStyleBuilder*    MsgStyleBuilder::s_instance = nullptr;
+
+
+MsgStyleBuilder::MsgStyleBuilder()
+{
+    // set some defaults
+}
+
+
+MsgStyleBuilder::~MsgStyleBuilder()
+{
+
+}
+
+
+MsgStyleBuilder&    MsgStyleBuilder::instance()
+{
+    if (s_instance==nullptr) {
+        s_instance = new MsgStyleBuilder;
+    }
+    return *s_instance;
+}
+
+MsgStyleBuilder&    MsgStyleBuilder::setColor(const QString& col)
+{
+    m_style[MsgStyleBuilder::Styles::COLOR] = col;
+    return *s_instance;
+}
+
+
+MsgStyleBuilder&    MsgStyleBuilder::setBackgroundColor(const QString &col)
+{
+    m_style[MsgStyleBuilder::Styles::BCK_COLOR] = col;
+    return *s_instance;
+}
+
+
+MsgStyleBuilder&    MsgStyleBuilder::setSelBackgroundColor(const QString &col)
+{
+    m_style[MsgStyleBuilder::Styles::SEL_BCK_COLOR] = col;
+    return *s_instance;
+}
+
+MsgStyleBuilder&    MsgStyleBuilder::setSelectionColor(const QString &col)
+{
+    m_style[MsgStyleBuilder::Styles::SEL_COLOR] = col;
+    return *s_instance;
+}
+
+
+QString     MsgStyleBuilder::strike()
+{
+    static char style[1024] = {0};
+
+    sprintf(style, "color: %s;"
+                   "background-color: %s;"
+                   "selection-color: %s;"
+                   "selection-background-color: %s;",
+            m_style[0].toLocal8Bit().constData(),
+            m_style[1].toLocal8Bit().constData(),
+            m_style[2].toLocal8Bit().constData(),
+            m_style[3].toLocal8Bit().constData()
+            );
+
+    QString ret_style(style);
+    return ret_style;
+}
+
 }
--- types.h
+++ types.h
@@ -6,35 +6,66 @@
 
 namespace msgboard {
 
-    enum MsgType
-    {
-        TIMER_ACTIVITY_MSG = 1,
-        USER_ACTIVITY_MSG = 2,
-        STATIC_ACTIVITY_MSG = 3
+enum MsgType
+{
+    TIMER_ACTIVITY_MSG = 1,
+    USER_ACTIVITY_MSG = 2,
+    STATIC_ACTIVITY_MSG = 3
+};
+
+enum LogType
+{
+    INFO_LOG = 1,
+    WARN_LOG = 2,
+    ERROR_LOG = 3,
+    UNKNOWN_LOG = 4
+};
+
+
+// info class
+class Msg
+{
+public:
+    Msg(const QString& msg, MsgType type, int timer);
+    virtual ~Msg(); // if we need to extend it but we won`t do it
+    QString m_msg;
+    MsgType m_type;
+    int m_timer; // -1 for non timer messages
+    int m_index; // reflects to the rows (or cols if needed )
+    int m_clicks; // false by default, user has not clicked it
+
+};
+
+
+
+class MsgStyleBuilder
+{
+private:
+    enum  Styles {
+        COLOR,
+        BCK_COLOR,
+        SEL_COLOR,
+        SEL_BCK_COLOR,
     };
 
-    enum LogType
-    {
-        INFO_LOG = 1,
-        WARN_LOG = 2,
-        ERROR_LOG = 3,
-        UNKNOWN_LOG = 4
-    };
+public:
+    static MsgStyleBuilder& instance();
 
 
-    // info class
-    class Msg
-    {
-    public:
-        Msg(const QString& msg, MsgType type, int timer);
-        virtual ~Msg(); // if we need to extend it but we won`t do it
-        QString m_msg;
-        MsgType m_type;
-        int m_timer; // -1 for non timer messages
-        int m_index; // reflects to the rows (or cols if needed )
-        int m_clicks; // false by default, user has not clicked it
+    MsgStyleBuilder& setColor(const QString& col);
+    MsgStyleBuilder& setBackgroundColor(const QString& col);
+    MsgStyleBuilder& setSelectionColor(const QString& col);
+    MsgStyleBuilder& setSelBackgroundColor(const QString& col);
 
-    };
+    QString         strike(void);
+
+private:
+    MsgStyleBuilder();
+    virtual ~MsgStyleBuilder();
+    static MsgStyleBuilder* s_instance;
+    QString m_style[4];
+
+};
 
 }
 
